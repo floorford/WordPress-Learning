0d6e22a12cacf62ef64cf7a6daa8b6407c6cf594
Flo Orford <floorford@Flos-MacBook-Pro.local>
2018-05-23 14:46:09 +0100

    altered the about content to use ACF

--- template-parts/about-content.php
+++ template-parts/about-content.php
@@ -11,7 +11,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' );
-		echo "<h2 class='entry-title'>This cute cat is only on the about page!</h2>";
 		?>
 	</header><!-- .entry-header -->
 
@@ -20,6 +19,28 @@
 	<div class="entry-content">
 		<?php
 		the_content();
+
+		if (get_field('company_name')) { ?>
+			<h2><?php the_field('company_name')?></h2>
+		<?php }
+
+		$image = get_field('company_logo');
+
+		if(get_field('company_logo')) { ?>
+		<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" /> <?php
+		}
+
+		if (get_field('postal_address')) { ?>
+			<h4> Come and visit our office:</h4><p><?php the_field('postal_address')?> </p>
+		<?php }
+
+		if (get_field('phone_number')) { ?>
+			<h4> Contact us here: </h4><p><?php the_field('phone_number')?> </p>
+		<?php }
+
+		if (get_field('contact_email_address')) { ?>
+			<h4> Send us an email: </h4><p><?php the_field('contact_email_address')?> </p>
+		<?php }
 
 		wp_link_pages( array(
 			'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
